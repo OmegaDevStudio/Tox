f0862ae6e4467a16fddef72c74b251e30b688d4f
Pinkstar-cyber <claudiutudor40@gmail.com>
2023-07-03 16:04:00 +0300

    penis

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+import json, aiohttp,asyncio,ujson
 
 from src import Report
 
@@ -8,14 +8,14 @@ reports: list[Report] = []
 def initial_load():
     for _, item in json['nodes'].items():
         reports.append(Report(item))
-
+elem_picks = []
+picks = [3]
 def show_options():
     for report in reports:
         if report.id == 3:
             print("Here are your options:\n")
             report.show_children()
-    elem_picks = []
-    picks = [3]
+
 
     while True:
         inp = input("\n\nPlease pick a value [>] ")
@@ -57,6 +57,31 @@ def show_options():
                             elem_picks.append(inp)
                             print(f"Report Complete! You picked {picks} and {elem_picks}")
                             return
-    
-initial_load()
-show_options()
+
+
+async def main():
+    initial_load()
+    show_options()
+    token = 'MTEwOTQzMjQ0ODQzNTEwNTg5NA.'
+    async with aiohttp.ClientSession(headers={'Authorization':token}, connector=aiohttp.TCPConnector(ssl=False, keepalive_timeout=10, limit=0, limit_per_host=0), trust_env=False, skip_auto_headers=None, json_serialize=ujson.dumps, auto_decompress=True) as session:
+        x = await session.get('https://discord.com/api/v9/users/@me')
+        if x.status == 401:
+            print('Invalid token.')
+        else:
+            msg = input('Enter message link [>] ')
+            msg_id = msg.split('/')[-1]
+            ch_id = msg.split('/')[-2]
+            report_payload = {
+                "version":"1.0",
+                "variant":"3",
+                "language":"en",
+                "breadcrumbs":picks,
+                "elements":{'pii_select':elem_picks},
+                "name":"message",
+                "channel_id":ch_id,
+                "message_id":msg_id
+                }
+            tox = await session.post('https://discord.com/api/v9/reporting/message', json=report_payload)
+            print(tox.status)
+            xx = await tox.json() ; print(xx)
+asyncio.run(main())
